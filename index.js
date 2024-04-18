cb7c36211e4c9f14ed0ea85cc7e061feb1d3846e
Alice <alicecodinglab@gmail.com>
2024-04-18 23:55:04 +0200

    first commit

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
- var express = require('express')
+const express = require('express')
+const cors = require('cors')
+const {Server} = require('socket.io')
+const {createServer} = require('node:http')
 
-var bodyParser = require("body-parser")
 const messages =[{
     name:"Alice",
     message:"Hi"
 }, {name:"Bob",message:"Hello" }]
 
-var app = express()
-app.use(bodyParser)
+const app = express()
+app.use(cors())
+app.use(express.json())
 app.use(express.static(__dirname))
+const server = createServer(app)
+const io = new Server(server,{
+    cors: {
+      origin:"*"
+    }
+  });
 
 app.get('/',(req,res)=>{
 
-    res.send('My ChatApp')
+    res.send('My Chat App API')
 
 })
-app.get('/messages',(req,res)=>{
-    res.send(messages)
+app.get('/text',(req,res)=>{
+
+    res.json({success:true, messages:'hola'})
 
 })
-app.post('/sendMessage',(req, res)=>{
-    messages.push(req.body.message)
-})
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('getMessage',()=>{       
+        io.emit('message',{
+            messages: messages
+        })
+    })
+    socket.on('sendMessage',(data)=>{
+        messages.push(data)
+        io.emit('message',{
+            messages: messages
+        })        
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+});
+
 
 const port = 3000
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`server is listening on port ${port}`)
 }) 
